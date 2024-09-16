b9e9e862c2ee95dfd6f3f3349816f53de33bdbe3
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2024-09-16 05:21:07 +0100

    register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,45 +2,56 @@ import Navbar from "../components/Navbar";
 
 const Register = () => {
   return (
-    <div className="bg-custom-gradient w-full px-5 lg:px-20">
+    <div className="bg-custom-gradient w-full px-5 md:px-10 lg:px-20">
       <Navbar />
-      <div className="flex flex-col lg:flex-row items-center gap-20 lg:py-32 pb-[300px]">
-        <div className=" text-[60px] text-white font-bold">
-          <h2>Become a <br />Deskstones <br /> Mentor</h2>
+      <div className="flex flex-col lg:flex-row items-center gap-10 lg:gap-20 py-16 lg:py-32 ">
+        {/* Headline Section */}
+        <div className="text-[40px] md:text-[50px] lg:text-[60px] text-white font-bold text-center lg:text-left">
+          <h2>
+            Become a <br /> Deskstones <br /> Mentor
+          </h2>
         </div>
-        <form action="" className="bg-white rounded-[20px] px-16 py-20 lg:w-[60%] ">
-            
-            <div className="flex gap-4 flex-col  text-[#6B6B6B]">
-                <p>First Name</p>
-              <input
-                type="text"
-                className="w-full  h-[60px] bg-[#F1F1F1] rounded-[5px] px-[12px] py-[6px] text-[#6B6B6B] outline-none"
-               
-              />
-              <p>Last Name</p>
-              <input
-                type="text"
-                className="bg-[#F1F1F1]  h-[60px] rounded-[5px] px-[12px] py-[6px] text-[#6B6B6B] outline-none w-full"
-              />
-              <p>Email</p>
-               <input
-                type="email"
-                className="w-full  h-[60px] bg-[#F1F1F1] rounded-[5px] px-[12px] py-[6px] text-[#6B6B6B] outline-none"
-              />
-               <p>Whatsapp Number</p>
-               <input
-                type="text"
-                className="w-full  h-[60px] bg-[#F1F1F1] rounded-[5px] px-[12px] py-[6px] text-[#6B6B6B] outline-none"
-               
-              />
-              <p>Role</p>
-               <input
-                type="text"
-                className="w-full  h-[60px] bg-[#F1F1F1] rounded-[5px] px-[12px] py-[6px] text-[#6B6B6B] outline-none"
-              />
-               <button className="w-full bg-[#10CC6F] px-5 py-3 rounded-[5px] text-white">Register</button>
-            </div>
-           
+
+        {/* Form Section */}
+        <form
+          action=""
+          className="bg-white rounded-[20px] px-8 py-10 md:px-12 md:py-16 lg:px-16 lg:py-20 lg:w-[60%] w-full"
+        >
+          <div className="flex gap-4 flex-col text-[#6B6B6B]">
+            <p>First Name</p>
+            <input
+              type="text"
+              className="w-full h-[50px] md:h-[60px] bg-[#F1F1F1] rounded-[5px] px-[12px] py-[6px] text-[#6B6B6B] outline-none"
+            />
+
+            <p>Last Name</p>
+            <input
+              type="text"
+              className="w-full h-[50px] md:h-[60px] bg-[#F1F1F1] rounded-[5px] px-[12px] py-[6px] text-[#6B6B6B] outline-none"
+            />
+
+            <p>Email</p>
+            <input
+              type="email"
+              className="w-full h-[50px] md:h-[60px] bg-[#F1F1F1] rounded-[5px] px-[12px] py-[6px] text-[#6B6B6B] outline-none"
+            />
+
+            <p>WhatsApp Number</p>
+            <input
+              type="text"
+              className="w-full h-[50px] md:h-[60px] bg-[#F1F1F1] rounded-[5px] px-[12px] py-[6px] text-[#6B6B6B] outline-none"
+            />
+
+            <p>Role</p>
+            <input
+              type="text"
+              className="w-full h-[50px] md:h-[60px] bg-[#F1F1F1] rounded-[5px] px-[12px] py-[6px] text-[#6B6B6B] outline-none"
+            />
+
+            <button className="w-full bg-[#10CC6F] px-5 py-3 rounded-[5px] text-white mt-4">
+              Register
+            </button>
+          </div>
         </form>
       </div>
     </div>
